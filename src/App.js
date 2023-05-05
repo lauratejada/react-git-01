1879ed4abba595bbb464d5df02e25e3d2cf10cda
LauraTejada <laura2191@hotmail.com>
2023-05-05 13:43:08 -0500

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Laura Tejada
         </a>
       </header>
     </div>
